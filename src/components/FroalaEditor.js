374cc08b8d56572fe0b0897a949be0a8cd3e9d3e
archanapd <archana@perfectzdigital.com>
2022-03-31 21:25:41 +0530

    froala media added

--- src/components/FroalaEditor.js
+++ src/components/FroalaEditor.js
@@ -3,8 +3,10 @@ import React from "react";
 import "froala-editor/js/froala_editor.pkgd.min.js";
 
 // Require Editor CSS files.
-import "froala-editor/css/froala_style.min.css";
-import "froala-editor/css/froala_editor.pkgd.min.css";
+import 'froala-editor/css/froala_style.min.css';
+import 'froala-editor/css/froala_editor.pkgd.min.css';
+import "froala-editor/js/plugins.pkgd.min.js";
+
 import FroalaEditorView from "react-froala-wysiwyg/FroalaEditorView";
 import FroalaEditor from "react-froala-wysiwyg";
 import "../App.css";
@@ -42,6 +44,7 @@ class EditorComponent extends React.Component {
         <FroalaEditor
           config={{
             charCounterCount: true,
+            placeholderText: 'Edit Your Content Here!'
           }}
           model={this.state.model}
           onModelChange={this.handleModelChange}
